361e635bc3f0279055a81753c66fe8e30b2685c9
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-04-14 15:01:29 +0200

    implemented toolbar view

--- app/src/main/java/app/fit/fitndflow/ui/activity/MainActivity.java
+++ app/src/main/java/app/fit/fitndflow/ui/activity/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new MainListFragment()).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.MainActContainer, new MainListFragment()).commit();
 
     }
 }
