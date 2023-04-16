73c1f8bb5de8adea5acce545df3c2309e70ada97
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-04-16 21:43:20 +0200

    implemented toolbar click listener

--- app/src/main/java/app/fit/fitndflow/ui/fragment/MainListFragment.java
+++ app/src/main/java/app/fit/fitndflow/ui/fragment/MainListFragment.java
@@ -2,13 +2,19 @@ package app.fit.fitndflow.ui.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.fit.fitndflow.R;
 import com.fit.fitndflow.databinding.MainListFragmentBinding;
 
 public class MainListFragment extends Fragment {
@@ -20,7 +26,36 @@ public class MainListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = MainListFragmentBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        ((AppCompatActivity)getActivity()).setSupportActionBar(binding.toolbar);
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.items,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.option1:
+                Toast.makeText(getContext(), "Menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.option2:
+                Toast.makeText(getContext(), "Añadir entrenamiento", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.option3:
+                Toast.makeText(getContext(), "Calendario", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
