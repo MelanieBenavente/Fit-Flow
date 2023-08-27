933c71bf031a25f9e072949ba689c46fcb3854ca
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-08-27 11:00:05 +0200

    refactor ItemModel to categroyModel and exerciseModel

---
+++ app/src/main/java/app/fit/fitndflow/domain/model/ExcerciseModel.java
@@ -0,0 +1,39 @@
+package app.fit.fitndflow.domain.model;
+
+public class ExcerciseModel {
+
+    private int id;
+
+    private String name;
+
+    private int primaryCategroy;
+
+    public ExcerciseModel(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrimaryCategroy() {
+        return primaryCategroy;
+    }
+
+    public void setPrimaryCategroy(int primaryCategroy) {
+        this.primaryCategroy = primaryCategroy;
+    }
+}
