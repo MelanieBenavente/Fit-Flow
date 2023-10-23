92b02c35993dd813861622d57409c2891555a16f
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-10-23 12:26:26 +0200

    finish splash activity

--- app/src/main/java/app/fit/fitndflow/domain/model/SerieModel.kt
+++ app/src/main/java/app/fit/fitndflow/domain/model/SerieModel.kt
@@ -2,4 +2,4 @@ package app.fit.fitndflow.domain.model
 
 import java.io.Serializable
 
-data class SerieModel(val reps: Int? = null, val kg: Double? = null, var exercise: ExerciseModel): Serializable
+data class SerieModel(val reps: Int? = null, val kg: Double? = null, var exercise: ExerciseModel? = null): Serializable
--- app/src/main/java/app/fit/fitndflow/ui/features/common/SplashActivity.java
+++ app/src/main/java/app/fit/fitndflow/ui/features/common/SplashActivity.java
@@ -14,5 +14,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
