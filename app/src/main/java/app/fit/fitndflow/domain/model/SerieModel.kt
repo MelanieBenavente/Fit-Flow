f65a82f27ab6aff7ea2152a254e55befb9f30fac
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-10-23 10:29:13 +0200

    minor fixes

---
+++ app/src/main/java/app/fit/fitndflow/domain/model/SerieModel.kt
@@ -0,0 +1,5 @@
+package app.fit.fitndflow.domain.model
+
+import java.io.Serializable
+
+data class SerieModel(val reps: Int? = null, val kg: Double? = null, var exercise: ExerciseModel): Serializable
