0ebf14d073cf3eaddfdab08d198810c1d3307390
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-12-14 11:18:39 +0100

    minor fixes

--- app/src/main/java/app/fit/fitndflow/ui/features/home/HomeFragment.java
+++ app/src/main/java/app/fit/fitndflow/ui/features/home/HomeFragment.java
@@ -106,7 +106,6 @@ public class HomeFragment extends CommonFragment<HomeViewModel> {
         final Observer<List<CategoryModel>> observer = new Observer<List<CategoryModel>>() {
             @Override
             public void onChanged(List<CategoryModel> categories) {
-                //todo!!!!!!!!!!!!!!!!!!!!!!!!!
                 if(categories == null || categories.isEmpty()){
                     binding.buttonPanel.setText("Añadir entrenamiento");
                     printEmptyView();
