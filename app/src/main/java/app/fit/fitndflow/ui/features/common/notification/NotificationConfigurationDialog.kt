9e131e728cc21a6058f5b2155841939dc41c22a7
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2024-03-06 12:07:08 +0100

    NotificationConfigurationDialog class migrated to kotlin

--- app/src/main/java/app/fit/fitndflow/ui/features/common/notification/NotificationConfigurationDialog.kt
+++ app/src/main/java/app/fit/fitndflow/ui/features/common/notification/NotificationConfigurationDialog.kt
@@ -1,4 +1,50 @@
 package app.fit.fitndflow.ui.features.common.notification
 
-class NotificationConfigurationDialog {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import app.fit.fitndflow.data.common.SharedPrefs
+import app.fit.fitndflow.ui.features.common.CommonDialogFragment
+import com.fit.fitndflow.databinding.DialogNotificationConfigurationBinding
+
+class NotificationConfigurationDialog : CommonDialogFragment() {
+
+    companion object {
+        @JvmField
+        val TAG = "NotificationConfigurationDialog"
+    }
+    private lateinit var binding: DialogNotificationConfigurationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        initListeners()
+        return view
+    }
+
+    private fun initListeners(){
+        binding.apply {
+            buttonPanel.setOnClickListener {
+                MyNotificationManager.launchNotificationSettings(requireContext())
+                dismissAllowingStateLoss()
+            }
+            closeButton.setOnClickListener { dismissAllowingStateLoss()
+            }
+            checkboxDontShowAgain.setOnCheckedChangeListener { buttonView, isChecked ->
+                SharedPrefs.get(requireContext()).saveDontShowNotification(isChecked)
+            }
+        }
+    }
+    override fun getBinding(): ViewBinding {
+        binding = DialogNotificationConfigurationBinding.inflate(layoutInflater)
+        return binding
+    }
+    override fun getBorderType(): Int {
+        return NAKED
+    }
 }
