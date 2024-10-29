5cff2faa841316d8eccd5262fd72edd6ea9e9b46
MelanieBenavente <lavienvert3@gmail.com>
2024-10-29 09:40:51 +0100

    cleanCache function created in CategoriesAndExercisesLocalDataSource

--- data-layer/src/main/java/com/fit/fitndflow/data/datasource/CategoriesAndExercisesLocalDataSource.kt
+++ data-layer/src/main/java/com/fit/fitndflow/data/datasource/CategoriesAndExercisesLocalDataSource.kt
@@ -5,11 +5,14 @@ import app.fit.fitndflow.domain.model.CategoryModel
 class CategoriesAndExercisesLocalDataSource {
     private var categoryListCachedResponse : List<CategoryModel>? = null
 
-    fun replaceAllDataFromCategoryListCache(categoryList: List<CategoryModel>?) {
+    fun replaceAllDataFromCategoryListCache(categoryList: List<CategoryModel>) {
         categoryListCachedResponse = categoryList
     }
 
     fun getAvailableCategoryListCache() : List<CategoryModel>? {
         return categoryListCachedResponse
+    }
+    fun cleanCache() {
+        categoryListCachedResponse = null
     }
 }
