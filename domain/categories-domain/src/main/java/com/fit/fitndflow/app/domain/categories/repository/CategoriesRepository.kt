0e4401cfea4136ac4ebba34e33bda63f0ce4947a
MelanieBenavente <lavienvert3@gmail.com>
2025-01-19 19:14:52 +0100

    CategoriesRepository modified

--- domain/categories-domain/src/main/java/com/fit/fitndflow/app/domain/categories/repository/CategoriesRepository.kt
+++ domain/categories-domain/src/main/java/com/fit/fitndflow/app/domain/categories/repository/CategoriesRepository.kt
@@ -4,18 +4,13 @@ import com.fit.fitndflow.app.domain.common.models.CategoryModel
 import com.fit.fitndflow.app.domain.common.repository.CommonRepository
 
 interface CategoriesRepository : CommonRepository {
-    @get:Throws(Exception::class)
-    val categoryList: List<CategoryModel?>?
-    @Throws(Exception::class)
-    fun addNewCategory(categoryName: String?, language: String?): List<CategoryModel?>?
-    @Throws(Exception::class)
-    fun modifyCategory(
-        categoryName: String?,
-        language: String?,
+    suspend fun getCategoryList(): List<CategoryModel>
+    suspend fun addNewCategory(categoryName: String, language: String): List<CategoryModel>
+    suspend fun modifyCategory(
+        categoryName: String,
+        language: String,
         categoryId: Int,
         imageUrl: String?
-    ): List<CategoryModel?>?
-
-    @Throws(Exception::class)
-    fun deleteCategory(integer: Int?): List<CategoryModel?>?
+    ): List<CategoryModel>
+    suspend fun deleteCategory(integer: Int): List<CategoryModel>
 }
