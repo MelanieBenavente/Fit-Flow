f8d1963a2ce7966e8170527be3342a9ac4fca24f
MelanieBenavente <lavienvert3@gmail.com>
2025-03-08 11:46:55 +0100

    database and mapper

--- data/common-data/src/main/java/app/fit/fitndflow/data/common/database/DataBase.kt
+++ data/common-data/src/main/java/app/fit/fitndflow/data/common/database/DataBase.kt
@@ -4,14 +4,17 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import app.fit.fitndflow.data.common.database.dao.CategoryDao
 import app.fit.fitndflow.data.common.database.dao.ExerciseDao
+import app.fit.fitndflow.data.common.database.dao.SerieDao
 import app.fit.fitndflow.data.common.database.entities.CategoryEntity
 import app.fit.fitndflow.data.common.database.entities.ExerciseEntity
-import app.fit.fitndflow.data.common.database.entities.SeriesEntity
+import app.fit.fitndflow.data.common.database.entities.SerieEntity
 
-@Database(entities = [CategoryEntity::class, ExerciseEntity::class, SeriesEntity::class], version = 1)
+@Database(entities = [CategoryEntity::class, ExerciseEntity::class, SerieEntity::class], version = 1)
 abstract class DataBase : RoomDatabase() {
 
     abstract fun getCategoryDao() : CategoryDao
 
     abstract fun getExerciseDao() : ExerciseDao
+
+    abstract fun getSerieDao() : SerieDao
 }
--- data/common-data/src/main/java/app/fit/fitndflow/data/common/database/mapper/DataBaseMapper.kt
+++ data/common-data/src/main/java/app/fit/fitndflow/data/common/database/mapper/DataBaseMapper.kt
@@ -3,10 +3,15 @@ package app.fit.fitndflow.data.common.database.mapper
 import app.fit.fitndflow.data.common.database.entities.CategoryEntity
 import app.fit.fitndflow.data.common.database.entities.CategoryWithExercisesEntity
 import app.fit.fitndflow.data.common.database.entities.ExerciseEntity
+import app.fit.fitndflow.data.common.database.entities.ExerciseWithSeriesEntity
+import app.fit.fitndflow.data.common.database.entities.SerieEntity
 import com.fit.fitndflow.app.domain.common.models.CategoryModel
 import com.fit.fitndflow.app.domain.common.models.ExerciseModel
+import com.fit.fitndflow.app.domain.common.models.SerieModel
 import com.fit.fitndflow.app.domain.common.models.StringInLanguagesModel
 
 fun CategoryEntity.toModel() = CategoryModel(id, StringInLanguagesModel(nameEs, nameEn))
 fun ExerciseEntity.toModel() = ExerciseModel(id, StringInLanguagesModel(nameEs, nameEn), mutableListOf(), null, null)
 fun CategoryWithExercisesEntity.toModel() = CategoryModel(category.id, StringInLanguagesModel(category.nameEs, category.nameEn), exercises.map { it.toModel() }.toMutableList() )
+fun SerieEntity.toModel() = SerieModel(id, reps, weight, false) //todo record
+fun ExerciseWithSeriesEntity.toModel() = ExerciseModel(exercise.id, StringInLanguagesModel(exercise.nameEs, exercise.nameEn), series.map { it.toModel() }.toMutableList(), null, null) //todo record
